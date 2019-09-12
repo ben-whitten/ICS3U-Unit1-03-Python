1a9875fb660ecfda6042ced8cde805a3ae51cbf4
Ubuntu <ubuntu@ip-172-31-27-17.ec2.internal>
2019-09-12 17:27:42 +0000

    added program

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: September 2019
+# This is the "Hello, world!" program, but nicer.
+
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
+    
